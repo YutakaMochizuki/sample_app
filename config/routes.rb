983bd3b9e7be62ff321dddf5f2ea5c8653d3323e
EC2 Default User <ec2-user@ip-172-31-44-0.ap-northeast-1.compute.internal>
2022-11-05 12:56:49 +0000

    [Finish]2-1

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   get '/top' => 'homes#top'
   patch 'lists/:id' => 'lists#update', as: 'update_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
 
 end
